69ac40396c1032727dd49d06b672bd55c85a4aae
JardaAdam <jardys23@gmail.com>
2024-11-13 18:55:03 +0100

    pridani prvni tabulky do models.py a po migraci vlozil 0001_initial.py do .gitignore

--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models import Model, CharField
+
 
 # Create your models here.
+# tabulka o dvou sloupcich
+class Genre(Model):
+    name = CharField(max_length=32, null=False, blank=False, unique=True)
+    """              delka, nesmi byt nevyplneno,nesmi byt prazdne pole pri odesilani formulare, unikatni   """
