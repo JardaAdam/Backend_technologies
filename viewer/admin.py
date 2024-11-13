34b6291645a2872f7c184c73d0a9df576aabc8fa
JardaAdam <jardys23@gmail.com>
2024-11-13 21:26:40 +0100

    konec hodiny 11/13/2024 models.py class: Genre,Country,Creators,Movies ; admin.py doplneny cesty ke class -> admin.site.register

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from viewer.models import *
+
 # Register your models here.
+
+admin.site.register(Country)
+admin.site.register(Creator)
+admin.site.register(Genre)
+admin.site.register(Movie)
--- viewer/models.py
+++ viewer/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.db.models import Model, CharField
+from django.db.models import Model, CharField, DateField, ForeignKey, SET_NULL, TextField, IntegerField, \
+    ManyToManyField, DateTimeField
 
 
 # Create your models here.
@@ -7,3 +8,76 @@ from django.db.models import Model, CharField
 class Genre(Model):
     name = CharField(max_length=32, null=False, blank=False, unique=True)
     """              delka, nesmi byt nevyplneno,nesmi byt prazdne pole pri odesilani formulare, unikatni   """
+
+    class Meta:
+        """ tato trida meni usporadani podle jmena abecedne pro zobrazeni v """
+        ordering = ['name']
+
+    def __repr__(self):
+        return f"Genre(name={self.name})"
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Country(Model):
+    name = CharField(max_length=32, null=False, blank=False, unique=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = "Countries"
+
+    def __repr__(self):
+        return f"Country(name={self.name})"
+
+    def __str__(self):
+        return self.name
+
+
+class Creator(Model):
+    first_name = CharField(max_length=32, null=True, blank=True)
+    last_name = CharField(max_length=32, null=True, blank=True)
+    date_of_birth = DateField(null=True, blank=True)
+    date_of_death = DateField(null=True, blank=True)
+    nationality = ForeignKey(Country, null=True, blank=True, on_delete=SET_NULL, related_name='creators_nationality')
+    """                           po vymazani zeme je nastavi prazdna kolonka, pristup k seznamu hercu z dane zeme"""
+    biography = TextField(null=True, blank=True)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name', 'date_of_birth']
+
+    def __repr__(self):
+        return f"Creator(first_name=({self.first_name}, last_name={self.first_name}, date_of_birth={self.date_of_birth})"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.date_of_birth})"
+        # "Martin Novák (1975-05-06)"
+
+
+class Movie(Model):
+    title_orig = CharField(max_length=64, null=False, blank=False)
+    title_cz = CharField(max_length=64, null=True, blank=True)
+    year = IntegerField()
+    length = IntegerField()
+    directors = ManyToManyField(Creator, blank=True, related_name='directing')
+    actors = ManyToManyField("viewer.Creator", blank=True, related_name='acting')
+    """ odkazuje na tabulku ktera je vytvorena az po teto tabulce"""
+    countries = ManyToManyField(Country, blank=True, related_name='movies')
+    genres = ManyToManyField(Genre, blank=True, related_name='movies')
+    description = TextField(blank=True)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+
+class Meta:
+    ordering = ['title_orig']
+
+
+def __repr__(self):
+    return f"Movie(title_orig={self.title_orig})"
+
+
+def __str__(self):
+    return f"{self.title_orig} ({self.year})"
