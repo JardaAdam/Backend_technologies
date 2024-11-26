aa6ced0c4e96249881c81db404de787b86647dae
JardaAdam <jardys23@gmail.com>
2024-11-26 18:56:24 +0100

    all add, migration complete, all works

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,20 +1,47 @@
 """ tento soubor si vytvorim rucne """
 from django.contrib.auth.forms import UserCreationForm
-from django import forms
+from django.db.transaction import atomic
+from django.forms import DateField, NumberInput, CharField, PasswordInput, Textarea
+from accounts.models import Profile
 
-from viewer import forms
+
 
 
 class SignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+        labels = {
+            'username': 'Uživatelské jméno',
+            'first_name': 'Jméno',
+            'last_name': 'Příjmení',
+            'email': 'email'
+        }
 
+    password1 = CharField(
+        widget=PasswordInput(attrs={'placeholder': 'Heslo'}),
+        label="Heslo"
+    )
+    password2 = CharField(
+        widget=PasswordInput(attrs={'placeholder': 'Heslo znovu'}),
+        label="Heslo znovu"
+    )
 
-    # password1 = forms.CharField(
-    #     widget=forms.PasswordInput(attrs={'placeholder': 'Heslo'}),
-    #     label="Heslo"
-    # )
-    # password2 = forms.CharField(
-    #     widget=forms.PasswordInput(attrs={'placeholder': 'Heslo znovu'}),
-    #     label="Heslo znovu"
-    # )
+    date_of_birth = DateField(widget=NumberInput(attrs={'type': 'date'}),
+                              label='Datum narození',
+                              required=False)
+    biography = CharField(widget=Textarea,
+                          label="Biografie",
+                          required=False)
+
+    @atomic
+    def save(self, commit=True):
+        self.instance.is_active = True
+        user = super().save(commit)  # Creates instance od User
+        date_of_birth = self.cleaned_data['date_of_birth']
+        biography = self.cleaned_data['biography']
+        profile = Profile(user=user,
+                          date_of_birth=date_of_birth,
+                          biography=biography)
+        if commit:
+            profile.save()
+        return user
