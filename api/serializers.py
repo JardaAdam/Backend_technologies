4ae5856ae0f3605d7aaa368ac202800400df7f00
JardaAdam <jardys23@gmail.com>
2024-12-03 23:56:53 +0100

    add API Movie and Creator create API.md

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
-from viewer.models import Movie
+from viewer.models import Movie, Creator
 
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['title_orig', 'title_cz', 'year']
+        #fields = ['title_orig', 'title_cz', 'year']
+        fields = '__all__'
+
+
+class CreatorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Creator
+        fields = '__all__'
